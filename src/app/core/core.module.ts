fc1a55d78380e08bbb581c68061e60dba30aef37
Rafał Trzop <rafaltrzop@gmail.com>
2020-05-13 21:30:43 +0200

    Add core module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from '@app/core/containers';
+
+@NgModule({
+  imports: [RouterModule],
+  declarations: [AppComponent],
+})
+export class CoreModule {}
