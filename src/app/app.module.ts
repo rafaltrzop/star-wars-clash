ade40245b6f0f12161ce4a8472c2a0495faf4ffd
Rafał Trzop <rafaltrzop@gmail.com>
2020-05-14 22:07:15 +0200

    Add Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from '@app/core';
+import { ClashModule } from '@app/clash';
 
 import { AppComponent } from '@app/core/containers';
 
 @NgModule({
-  imports: [BrowserModule, AppRoutingModule, CoreModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    CoreModule,
+    ClashModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
