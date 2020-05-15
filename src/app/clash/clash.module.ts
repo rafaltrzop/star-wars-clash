667d9614f8e85375b733a69b92512a62012509ed
Rafał Trzop <rafaltrzop@gmail.com>
2020-05-15 21:29:10 +0200

    Add material module

--- src/app/clash/clash.module.ts
+++ src/app/clash/clash.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 
+import { MaterialModule } from '@app/material';
 import { ClashRoutingModule } from '@app/clash/clash-routing.module';
 
 import { ClashPageComponent } from '@app/clash/containers';
 import { CharacterSelectionComponent } from '@app/clash/components';
 
 @NgModule({
-  imports: [ClashRoutingModule],
+  imports: [MaterialModule, ClashRoutingModule],
   declarations: [ClashPageComponent, CharacterSelectionComponent],
 })
 export class ClashModule {}
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { MatRadioModule } from '@angular/material/radio';
+
+@NgModule({
+  imports: [MatRadioModule],
+  exports: [MatRadioModule],
+})
+export class MaterialModule {}
