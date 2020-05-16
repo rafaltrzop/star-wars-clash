fb2bc9c2a79a8370a8f268233db309bbfcb88c5c
Rafał Trzop <rafaltrzop@gmail.com>
2020-05-16 13:25:43 +0200

    Add @ngrx/store-devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from '@app/core';
 
 import { AppComponent } from '@app/core/containers';
+
+import { environment } from '../environments/environment';
 
 @NgModule({
   imports: [
@@ -25,6 +28,11 @@ import { AppComponent } from '@app/core/containers';
         },
       }
     ),
+    StoreDevtoolsModule.instrument({
+      name: 'Star Wars Clash',
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
     CoreModule,
   ],
   providers: [],
