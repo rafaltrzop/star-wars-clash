14ca3fa9a32cb3ea3f9feaf12b0fca31d592b448
Rafał Trzop <rafaltrzop@gmail.com>
2020-05-16 12:46:37 +0200

    Add @ngrx/store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from '@app/core';
@@ -12,6 +13,7 @@ import { AppComponent } from '@app/core/containers';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    StoreModule.forRoot({}, {}),
     CoreModule,
   ],
   providers: [],
