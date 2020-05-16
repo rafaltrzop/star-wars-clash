f20c1c05f27a82bcc6636fc2b053e9a3bb824626
Rafał Trzop <rafaltrzop@gmail.com>
2020-05-16 12:45:49 +0200

    Remove unnecessary module import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from '@app/core';
-import { ClashModule } from '@app/clash';
 
 import { AppComponent } from '@app/core/containers';
 
@@ -14,7 +13,6 @@ import { AppComponent } from '@app/core/containers';
     BrowserAnimationsModule,
     AppRoutingModule,
     CoreModule,
-    ClashModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
